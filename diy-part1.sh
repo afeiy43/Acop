ab8195191aafb1b5c41a431558e1902200386824
afeiy43 <afeiyafeiy43@gmail.com>
2025-12-04 15:22:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,8 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >>feeds.conf.default
+echo 'src-git nat6-helper https://github.com/Ausaci/luci-app-nat6-helper.git' >>feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
