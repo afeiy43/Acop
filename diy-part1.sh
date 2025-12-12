932f530700dae294add718ab247bb90fdd20bafc
afeiy43 <afeiyafeiy43@gmail.com>
2025-12-12 08:11:20 +0800

    Comment out multiple feed source lines
    Comment out feed source additions for various packages.

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >>feeds.conf.default
-echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >>feeds.conf.default
+#echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
+#echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
